04676d32da1384956609a683427f27c26fcc22db
manikanta222 <manikanta222@gmail.com>
2025-04-04 22:44:01 +0530

    refactor_few_directories

--- eks/vpc.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source          = "terraform-aws-modules/vpc/aws"
-  name            = var.eks_cluster_name
-  cidr            = "10.0.0.0/16"
-  enable_nat_gateway = true
-
-  public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
-}
